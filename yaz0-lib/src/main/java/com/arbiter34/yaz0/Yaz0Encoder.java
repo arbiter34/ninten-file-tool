56b42e8fe4085c914ebcded1c8db068efdd19ca0
Travis Alpers <talpers@sofi.org>
2018-01-08 15:05:51 -0700

    Temp commit, still broken

--- yaz0-lib/src/main/java/com/arbiter34/yaz0/Yaz0Encoder.java
+++ yaz0-lib/src/main/java/com/arbiter34/yaz0/Yaz0Encoder.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Yaz0Encoder {
     private static final long HEADER_SIZE = 0x10;
     private static final int MAX_SEARCH_SIZE = 0x111;
-    private static final int REQUIRES_THIRD_BYTE = 0x12;
+    private static final int MAX_BACK_REFERENCE = 0xFFF;
+    private static final int REQUIRES_THIRD_BYTE = 0x11;
 
     public static BinaryAccessFile encode(final BinaryAccessFile in, final String outPath) throws IOException {
         final BinaryAccessFile out = new BinaryAccessFile(outPath, "rw");
@@ -41,34 +43,41 @@ public class Yaz0Encoder {
                 headerPos = 0;
                 out.seek(headerPointer + 1);
             }
-            int searchSize = MAX_SEARCH_SIZE;
             long searchStart = in.getFilePointer();
-            byte[] searchBytes = new byte[searchSize];
+            byte[] searchBytes = new byte[MAX_SEARCH_SIZE];
+
+            // Read max search chunk then seek back
             in.read(searchBytes);
-            in.seek(in.getFilePointer() - searchSize);
+            in.seek(searchStart);
+
+            // Attempt to find largest common subarray between search chunk and out.getFilePointer() - MAX_BACK_REFERENCE
             Pair<Long, Integer> offsetBestPair = offsetOf(out, searchBytes);
-            if (offsetBestPair.getLeft() == -1) {
-                // No bytes matched -> seek search start -> read byte -> write -> set header bit
-                out.write(in.read());
-                header |= (0x80 >>> headerPos);
-                headerPos++;
+            // Don't back reference if it'll take more bytes to represent than we're saving
+            if (offsetBestPair.getRight() < 3) {
+                    out.write(in.read());
+                    header |= (0x80 >>> headerPos);
+                    headerPos++;
                 continue;
             }
 
             int best = offsetBestPair.getRight();
             final long offset = offsetBestPair.getLeft();
+            if (best > 10) {
+                best = best;
+            }
             // We found bytes -> seek input over matched bytes
             in.seek(searchStart + best);
+
             int size = best;
             boolean found = false;
             int count = 0;
             while (size > 0) {
                 byte[] next = new byte[size];
                 count = 0;
-                while ((count * next.length) < MAX_SEARCH_SIZE) {
+                while (best + (count * next.length) < MAX_SEARCH_SIZE) {
                     in.read(next);
-
-                    if (Arrays.equals(Arrays.copyOfRange(searchBytes, searchBytes.length - size, searchBytes.length), next)) {
+                    assert(best - (best - size) == next.length);
+                    if (Arrays.equals(Arrays.copyOfRange(searchBytes, 0, best - (best - size)), next)) {
                         found = true;
                         count++;
                         continue;
@@ -80,7 +89,7 @@ public class Yaz0Encoder {
                 }
                 size--;
             }
-            if (size > 1) {
+            if (size > 15) {
                 size = size;
             }
             int backReference = (int)(out.getFilePointer() - offset + 2 - 1);
@@ -88,39 +97,91 @@ public class Yaz0Encoder {
             in.seek(searchStart + best);
             headerPos++;    // 0 bit in header for reference byte
             if (best < REQUIRES_THIRD_BYTE) {
-                int toWrite = ((searchSize - 0x02) << 12) | backReference;
+                int toWrite = ((best - 0x02) << 12) | (backReference & 0x0000000000000FFF);
                 out.writeShort(toWrite);
             } else {
-                out.writeShort(backReference);
-                out.write(searchSize);
+                out.writeShort((backReference & 0x0000000000000FFF));
+                out.write(best);
             }
-            headerPos++;
         }
 
         return out;
     }
 
     private static Pair<Long, Integer> offsetOf(final BinaryAccessFile file, byte[] searchBytes) throws IOException {
-        long currentPosition = file.getFilePointer();
-        if (currentPosition - searchBytes.length < HEADER_SIZE) {
-            file.seek(currentPosition);
-            return Pair.of(-1L, -1);
+        long mark = file.getFilePointer();
+        int backSearchSize = MAX_BACK_REFERENCE;
+        while (file.getFilePointer() - backSearchSize < HEADER_SIZE) {
+            backSearchSize--;
+            if (backSearchSize == 0) {
+                return Pair.of(-1L, -1);
+            }
         }
 
-        byte[] buffer = new byte[MAX_SEARCH_SIZE];
-        Pair<Long, Integer> longest = longestSubarray(searchBytes, buffer);
-        if (longest.getLeft() != -1) {
-            byte[] found = Arrays.copyOf(searchBytes, longest.getRight());
-            long offset = currentPosition + longest.getLeft();
-            offsetCache.put(Arrays.hashCode(found), offset);
+        if (backSearchSize == MAX_BACK_REFERENCE) {
+            backSearchSize = backSearchSize;
         }
-        file.seek(currentPosition);
+
+        byte[] buffer = new byte[backSearchSize];
+        // Seek back backSearchSize then read backSearchSize many bytes (restoring position)
+        file.seek(file.getFilePointer() - buffer.length);
+        file.read(buffer);
+
+        // Find longest subarray offset and size
+        Pair<Long, Integer> longest = lcSubarray(buffer, searchBytes);
+        if (longest.getLeft() != -1) {
+            // Index found in buffer is how many bytes into the search chunk we skip
+            long offset = file.getFilePointer() - buffer.length + longest.getLeft();
+            longest.setLeft(offset);
+        }
+        file.seek(mark);
         return longest;
     }
 
-    private static Map<Integer, Long> offsetCache = new HashMap<>();
+    private static Pair<Long, Integer> lcSubarray(final byte[] haystack, final byte[] needle) {
+        long bestIndex = -1;
+        int best = -1;
+        int startIndex = 0;
+        for (int i = 1; i < needle.length; i++) {
+            final int indexOf = indexOf(haystack, Arrays.copyOf(needle, i), startIndex);
+            if (indexOf == -1) {
+                return Pair.of(bestIndex, best);
+            }
+            startIndex = indexOf;
+            best = i;
+            bestIndex = indexOf;
+        }
+        return Pair.of(bestIndex, best);
+    }
 
-    public static Pair<Long, Integer> longestSubarray(byte[] source, byte[] target) {
+    private static int indexOf(byte[] source, byte[] target, int start) {
+        if (target.length > source.length) {
+            return -1;
+        }
+        for (int i = start; i <= source.length - target.length; i++) {
+            int targetPos = 0;
+            while (source[i + targetPos] == target[targetPos]) {
+                targetPos++;
+                if (targetPos == target.length) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static Byte[] toObject(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+        Byte[] newBytes = new Byte[bytes.length];
+        for (int i = 0; i < newBytes.length; i++) {
+            newBytes[i] = bytes[i];
+        }
+        return newBytes;
+    }
+
+    private static Pair<Long, Integer> longestSubarray(byte[] source, byte[] target) {
         if (source.length == 0 || target.length == 0) {
             return Pair.of(-1L, -1);
         }
@@ -148,7 +209,7 @@ public class Yaz0Encoder {
                 d[j] = cost;
 
                 if (cost > maxLen) {
-                    location = j;
+                    location = i;
                     maxLen = cost;
                 }
             } // for {}
